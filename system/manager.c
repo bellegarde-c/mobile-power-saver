157130843080389d8cca9511579a60ce7c58a44c
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-12-08 01:58:17 +0100

    Revert "systemd: Remove binder support"
    This reverts commit bb4baefbd1425ca5b3520c871f8ef4c1271d8bcf.

--- system/manager.c
+++ system/manager.c
@@ -16,6 +16,10 @@
 #include "logind.h"
 #include "manager.h"
 
+#ifdef BINDER_ENABLED
+#include "binder.h"
+#endif
+
 #ifdef WIFI_ENABLED
 #include "wifi.h"
 #endif
@@ -27,6 +31,9 @@
 #define APPLY_DELAY 500
 
 struct _ManagerPrivate {
+#ifdef BINDER_ENABLED
+    Binder *binder;
+#endif
     Cpufreq *cpufreq;
     Devfreq *devfreq;
     KernelSettings *kernel_settings;
@@ -84,11 +91,16 @@ on_screen_state_changed (Logind logind,
 
     if (self->priv->screen_off_power_saving) {
         bus_screen_state_changed (bus_get_default (), screen_on);
+
         devfreq_set_powersave (self->priv->devfreq, !screen_on);
         kernel_settings_set_powersave (self->priv->kernel_settings, !screen_on);
+
 #ifdef WIFI_ENABLED
         if (self->priv->radio_power_saving)
             wifi_set_powersave (self->priv->wifi, !screen_on);
+#endif
+#ifdef BINDER_ENABLED
+        binder_set_powersave (self->priv->binder, !screen_on);
 #endif
 
         if (screen_on) {
@@ -146,6 +158,9 @@ set_power_profile (Manager      *self,
 
     cpufreq_set_governor (self->priv->cpufreq, governor);
     devfreq_set_governor (self->priv->devfreq, governor);
+#ifdef BINDER_ENABLED
+    binder_set_power_profile (self->priv->binder, power_profile);
+#endif
 }
 
 static void
@@ -316,6 +331,10 @@ manager_dispose (GObject *manager)
         self->priv->suspend_bluetooth_services
     );
 
+
+#ifdef BINDER_ENABLED
+    g_clear_object (&self->priv->binder);
+#endif
     g_clear_object (&self->priv->cpufreq);
     g_clear_object (&self->priv->devfreq);
     g_clear_object (&self->priv->kernel_settings);
@@ -368,6 +387,9 @@ manager_init (Manager *self)
 {
     self->priv = manager_get_instance_private (self);
 
+#ifdef BINDER_ENABLED
+    self->priv->binder = BINDER (binder_new ());
+#endif
     self->priv->cpufreq = CPUFREQ (cpufreq_new ());
     self->priv->devfreq = DEVFREQ (devfreq_new ());
     self->priv->kernel_settings = KERNEL_SETTINGS (kernel_settings_new ());
